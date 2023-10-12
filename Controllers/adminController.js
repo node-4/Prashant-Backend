3f6e6884e7737f1f9fd9838369ea61d5e2599988
rubiyadav <rubiflyeis@gmail.com>
2023-10-12 15:33:52 +0530

    update

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 var newOTP = require("otp-generators");
 const nodemailer = require("nodemailer");
 const authConfig = require("../configs/auth.config");
+const notification = require("../models/notification");
 exports.registration = async (req, res) => {
     const { phone, email } = req.body;
     try {
@@ -211,3 +212,89 @@ exports.deleteUser = async (req, res) => {
         return res.status(500).send({ msg: "internal server error", error: err.message, });
     }
 };
+exports.sendNotification = async (req, res) => {
+    try {
+        const admin = await User.findById({ _id: req.user._id });
+        if (!admin) {
+            return res.status(404).json({ status: 404, message: "Admin not found" });
+        } else {
+            if (req.body.total == "ALL") {
+                let userData = await User.find({ userType: req.body.sendTo });
+                if (userData.length == 0) {
+                    return res.status(404).json({ status: 404, message: "User not found" });
+                } else {
+                    for (let i = 0; i < userData.length; i++) {
+                        let obj = {
+                            userId: userData[i]._id,
+                            title: req.body.title,
+                            body: req.body.body,
+                            date: req.body.date,
+                            image: req.body.image,
+                            time: req.body.time,
+                        }
+                        await notification.create(obj)
+                    }
+                    let obj1 = {
+                        userId: admin._id,
+                        title: req.body.title,
+                        body: req.body.body,
+                        date: req.body.date,
+                        image: req.body.image,
+                        time: req.body.time,
+                    }
+                    await notification.create(obj1)
+                    return res.status(200).json({ status: 200, message: "Notification send successfully." });
+                }
+            }
+            if (req.body.total == "SINGLE") {
+                let userData = await User.findById({ _id: req.body._id, userType: req.body.sendTo });
+                if (!userData) {
+                    return res.status(404).json({ status: 404, message: "User not found" });
+                } else {
+                    let obj = {
+                        userId: userData._id,
+                        title: req.body.title,
+                        body: req.body.body,
+                        date: req.body.date,
+                        image: req.body.image,
+                        time: req.body.time,
+                    }
+                    let data = await notification.create(obj)
+                    if (data) {
+                        let obj1 = {
+                            userId: admin._id,
+                            title: req.body.title,
+                            body: req.body.body,
+                            date: req.body.date,
+                            image: req.body.image,
+                            time: req.body.time,
+                        }
+                        await notification.create(obj1)
+                        return res.status(200).json({ status: 200, message: "Notification send successfully.", data: data });
+                    }
+                }
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(501).send({ status: 501, message: "server error.", data: {}, });
+    }
+}
+exports.allNotification = async (req, res) => {
+    try {
+        const admin = await User.findById({ _id: req.user._id });
+        if (!admin) {
+            return res.status(404).json({ status: 404, message: "Admin not found" });
+        } else {
+            let findNotification = await notification.find({ userId: admin._id }).populate('userId');
+            if (findNotification.length == 0) {
+                return res.status(404).json({ status: 404, message: "Notification data not found successfully.", data: {} })
+            } else {
+                return res.status(200).json({ status: 200, message: "Notification data found successfully.", data: findNotification })
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(501).send({ status: 501, message: "server error.", data: {}, });
+    }
+}
--- routes/adminAuth.route.js
+++ routes/adminAuth.route.js
@@ -8,4 +8,6 @@ module.exports = (app) => {
     app.get("/api/v1/admin/getAllUser", auth.getAllUser);
     app.get("/api/v1/admin/viewUser/:id", [authJwt.verifyToken], auth.viewUser);
     app.delete("/api/v1/admin/:id", [authJwt.verifyToken], auth.deleteUser);
+    app.post("/api/v1/notification/sendNotification", authJwt.verifyToken, auth.sendNotification);
+    app.get("/api/v1/notification/allNotification", authJwt.verifyToken, auth.allNotification);
 }
