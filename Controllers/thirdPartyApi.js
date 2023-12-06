be9db37009e87e725345d59837da736a3b1f1f71
rubiyadav <rubiflyeis@gmail.com>
2023-12-06 10:47:40 +0530

    update

--- Controllers/thirdPartyApi.js
+++ Controllers/thirdPartyApi.js
@@ -8,6 +8,7 @@ const authConfig = require("../configs/auth.config");
 const axios = require('axios');
 const crypto = require('crypto');
 const { log } = require("console");
+const qs = require('querystring');
 
 exports.networkStorePayment = async (req, res) => {
     try {
@@ -120,9 +121,101 @@ exports.query = async (req, res) => {
     }
 }
 
+// exports.query1 = async (req, res) => {
+//     try {
+//         // Extract data from the request body
+//         const { merNo,
+//             amount,
+//             billNo,
+//             currency,
+//             language,
+//             returnURL,
+//             notifyUrl,
+//             tradeUrl,
+//             lastName,
+//             firstName,
+//             country,
+//             state,
+//             city,
+//             address,
+//             zipCode,
+//             email,
+//             phone,
+//             shippingFirstName,
+//             shippingLastName,
+//             shippingCountry,
+//             shippingState,
+//             shippingCity,
+//             shippingAddress,
+//             shippingZipCode,
+//             shippingEmail,
+//             shippingPhone,
+//             cardNum,
+//             year,
+//             month,
+//             cvv2,
+//             cardBank,
+//             productInfo,
+//             nationalCode,
+//             ip,
+//             md5Info,
+//             acceptLanguage,
+//             userAgent,
+//             timeZone,
+//             javascriptEnabled, } = req.body;
+//         var url = `https://testurl.carespay.com:28081/carespay/pay`;
+//         const qs = require('querystring');
+//         req.body.md5Info = merNo + billNo + currency + amount + returnURL + '^Qdb}Kzy'
+//         console.log(req.body);
+//         const formData = qs.stringify(req.body);
+//         axios.post(url, formData, {
+//             headers: {
+//                 'Content-Type': 'application/x-www-form-urlencoded',
+//             },
+//         }).then(function (response) {
+//             console.log(response.data);
+//             resolve(response)
+//             return res.status(200).send({ msg: "Data Payment", data: response, });
+
+//         })
+//             .catch(function (error) {
+//                 return res.status(501).send({ msg: "error", data: error, });
+//             });
+
+
+
+
+
+
+
+
+
+//         // axios({
+//         //     method: 'post',
+//         //     url: url,
+//         //     data: req.body
+//         // }).then(function (response) {
+//         //     console.log(response.data);
+//         //     resolve(response)
+//         //     return res.status(200).send({ msg: "Data Payment", data: response, });
+
+//         // })
+//         //     .catch(function (error) {
+//         //         return res.status(501).send({ msg: "error", data: error, });
+//         //     });
+
+
+//         // const response = await axios.post('https://testurl.carespay.com:28081/carespay/pay', req.body);
+//         // // Send the response back to the client
+//         // return res.json(response.data);
+//     } catch (error) {
+//         console.error(error);
+//         return res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// };
+
 exports.query1 = async (req, res) => {
     try {
-        // Extract data from the request body
         const { merNo,
             amount,
             billNo,
@@ -157,56 +250,32 @@ exports.query1 = async (req, res) => {
             productInfo,
             nationalCode,
             ip,
-            md5Info,
             acceptLanguage,
             userAgent,
             timeZone,
             javascriptEnabled, } = req.body;
+        const md5String = merNo + billNo + currency + amount + returnURL + '^Qdb}Kzy';
+        const md5Info = crypto.createHash('md5').update(md5String).digest('hex');
+        req.body.md5Info = md5Info;
         var url = `https://testurl.carespay.com:28081/carespay/pay`;
-        const qs = require('querystring');
-        req.body.md5Info = merNo + billNo + currency + amount + returnURL + '^Qdb}Kzy'
-        console.log(req.body);
         const formData = qs.stringify(req.body);
         axios.post(url, formData, {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
         }).then(function (response) {
-            console.log(response.data);
-            resolve(response)
-            return res.status(200).send({ msg: "Data Payment", data: response, });
+            console.log(response);
+            const responseData = {
+                code: response.data.code,
+                message: response.data.message,
+                orderNo: response.data.orderNo,
+            };
 
-        })
-            .catch(function (error) {
-                return res.status(501).send({ msg: "error", data: error, });
-            });
-
-
-
-
-
-
-
-
-
-        // axios({
-        //     method: 'post',
-        //     url: url,
-        //     data: req.body
-        // }).then(function (response) {
-        //     console.log(response.data);
-        //     resolve(response)
-        //     return res.status(200).send({ msg: "Data Payment", data: response, });
-
-        // })
-        //     .catch(function (error) {
-        //         return res.status(501).send({ msg: "error", data: error, });
-        //     });
-
-
-        // const response = await axios.post('https://testurl.carespay.com:28081/carespay/pay', req.body);
-        // // Send the response back to the client
-        // return res.json(response.data);
+            return res.status(200).send({ msg: "Data Payment", data: responseData });
+        }).catch(function (error) {
+            console.error(error);
+            return res.status(501).send({ msg: "error", data: error.message });
+        });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ error: 'Internal Server Error' });
